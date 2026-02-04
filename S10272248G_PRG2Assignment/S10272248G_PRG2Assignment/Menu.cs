cf5449e15b15dd0019d5d525eaa6518373dffa6d
Cai Yun Sherlyn Yeo <s10272248@connect.np.edu.sg>
2026-02-04 19:53:02 +0800

    Added classes

---
+++ S10272248G_PRG2Assignment/S10272248G_PRG2Assignment/Menu.cs
@@ -0,0 +1,56 @@
+﻿// Student Number : S10272248G
+// Student Name : Sherlyn Yeo Cai Yun
+// Partner Name : Suhailah 
+
+using System;
+using System.Collections.Generic;
+
+namespace Gruberoo
+{
+    class Menu
+    {
+        public string MenuId { get; set; }
+        public string MenuName { get; set; }
+        public List<FoodItem> FoodItemList { get; private set; } = new List<FoodItem>();
+        public Menu() { }
+
+        public Menu(string id, string name)
+        {
+            MenuId = id;
+            MenuName = name;
+        }
+
+        public void AddFoodItem(FoodItem fi)
+        {
+            if (fi != null)
+                FoodItemList.Add(fi);
+        }
+
+        public bool RemoveFoodItem(FoodItem fi)
+        {
+            if (fi == null) return false;
+            return FoodItemList.Remove(fi);
+        }
+
+        public void DisplayFoodItems()
+        {
+            if (FoodItemList.Count == 0)
+            {
+                Console.WriteLine("  (No food items)");
+                return;
+            }
+
+            int i = 1;
+            foreach (FoodItem fi in FoodItemList)
+            {
+                Console.WriteLine($"{i}. {fi}");
+                i++;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Menu: {MenuName} ({MenuId})";
+        }
+    }
+}
---
+++ S10272248G_PRG2Assignment/S10272248G_PRG2Assignment/Restaurant.cs
@@ -0,0 +1,78 @@
+﻿// Student Number : S10272248G
+// Student Name : Sherlyn Yeo Cai Yun
+// Partner Name : Suhailah 
+
+using System;
+using System.Collections.Generic;
+
+namespace Gruberoo
+{
+    class Restaurant
+    {
+        public string RestaurantId { get; set; }
+        public string RestaurantName { get; set; }
+        public string RestaurantEmail { get; set; }
+
+        public List<Menu> MenuList { get; private set; } = new List<Menu>();
+        public List<SpecialOffer> SpecialOfferList { get; private set; } = new List<SpecialOffer>();
+        public Queue<Order> OrderQueue { get; private set; } = new Queue<Order>();
+        public Stack<Order> RefundStack { get; private set; } = new Stack<Order>();
+
+        public Restaurant() { }
+
+        public Restaurant(string id, string name, string email)
+        {
+            RestaurantId = id;
+            RestaurantName = name;
+            RestaurantEmail = email;
+        }
+
+        public void DisplayMenu()
+        {
+            if (MenuList.Count == 0)
+            {
+                Console.WriteLine("(No menus)");
+                return;
+            }
+
+            foreach (Menu m in MenuList)
+            {
+                Console.WriteLine(m);
+                m.DisplayFoodItems();
+            }
+        }
+
+        public void DisplayOrders()
+        {
+            if (OrderQueue.Count == 0)
+            {
+                Console.WriteLine("(No orders)");
+                return;
+            }
+
+            foreach (Order o in OrderQueue)
+            {
+                Console.WriteLine(o);
+            }
+        }
+
+        public void DisplaySpecialOffers()
+        {
+            if (SpecialOfferList.Count == 0)
+            {
+                Console.WriteLine("(No offers)");
+                return;
+            }
+
+            foreach (SpecialOffer so in SpecialOfferList)
+            {
+                Console.WriteLine(so);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Restaurant: {RestaurantName} ({RestaurantId})";
+        }
+    }
+}
